f1b121ad57255c85638d1aa9c687eef158379de0
Andrew Smith <andrw.l.smth@gmail.com>
2017-03-24 13:22:24 -0400

    Restructure models/add migtration

--- form/models.py
+++ form/models.py
@@ -1,2 +1,14 @@
 from django.db import models
-from django.utils import timezone
+
+class Buyer(models.Model):
+    buyer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    phone = models.IntegerField(max_length=10)
+
+class Meta:
+    db_table=u'Buyer'
+
+    def __unicode__(self):
+        return u"%d %s %s %s %d" % (self.pk, self.first_name, self.last_name, self.email,self.phone)
