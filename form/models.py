4463b4728ea9d01946740e58e068032210018056
Devon <Devon Davis>
2017-04-23 19:07:00 -0400

    Changed back to foreign keys, user side of the project works as specified

--- form/models.py
+++ form/models.py
@@ -32,21 +32,21 @@ class Buyer(models.Model):
 	phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=10)
 
 class BuyerRole(models.Model):
-	buyer_id = models.IntegerField(default=0)
-	role_id = models.IntegerField(default=0)
+	buyer_id = models.ForeignKey('buyer')
+	role_id = models.ForeignKey('role')
 
 class BuyerDeliveryInfo(models.Model):
-	buyer_id = models.IntegerField()
+	buyer_id = models.ForeignKey('buyer')
 	building_name = models.CharField(max_length=30)
 	room_number = models.CharField(max_length=10)
 
 class Order(models.Model):
 	order_id = models.AutoField('order')
 	order_id.primary_key = True
-	buyer_id = models.IntegerField(default=0)
-	sock_id = models.IntegerField(default=0)
-	charity_id = models.IntegerField(default=0)
-	delivery_id = models.IntegerField(default=0)
+	buyer_id = models.ForeignKey('buyer')
+	sock_id = models.ForeignKey('sock')
+	charity_id = models.ForeignKey('charity')
+	delivery_id = models.ForeignKey('delivery')
 	
 #comprehsnive fall back option if we cant get formset to work
 class Comprehensive(models.Model):
@@ -58,8 +58,8 @@ class Comprehensive(models.Model):
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{10,10}$', message="Phone number must be entered in the format: '+999999999'. 10 digits allowed.")
 	phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=10) # validators should be a list
 	sock_id = models.IntegerField(choices= choices, default=0, verbose_name='Sock Choice')
-	charity_id = models.IntegerField(choices= choices2, default=0, verbose_name='Chairty')
-	role_id = models.IntegerField(choices= choices3, default=0, verbose_name='Role')
+	charity_id = models.IntegerField(choices= choices3, default=0, verbose_name='Charity')
+	role_id = models.IntegerField(choices= choices2, default=0, verbose_name='Role')
 	delivery_id = models.IntegerField(choices= choices4, default=0, verbose_name='Delivery Option')
 	building_name = models.CharField(max_length=30,null=True, blank=True)
 	room_number = models.CharField(max_length=3,null=True, blank=True)
--- form/views.py
+++ form/views.py
@@ -52,42 +52,31 @@ def user(request):
 				buyer.email = data['email']
 				buyer.phone_number = data['phone_number']
 				buyer.save()
-				buyerRole.buyer_id = buyer.buyer_id
-				buyerRole.role_id = data['role_id']
+				buyerRole.buyer_id = Buyer.objects.all().get(email = data['email'])
+				buyerRole.role_id = Role.objects.all().get(role_id = data['role_id'])
 				buyerRole.save()
 				if data['delivery_id'] == 2:
-					buyerDelInfo.buyer_id = buyer.buyer_id
+					buyerDelInfo.buyer_id = Buyer.objects.all().get(email = data['email'])
 					buyerDelInfo.building_name = data['building_name']
 					buyerDelInfo.room_number = data['room_number']
 					buyerDelInfo.save()
-				order.buyer_id = buyer.buyer_id
-				order.sock_id = data['sock_id']
-				order.charity_id = data['charity_id']
-				order.delivery_id = data['delivery_id']
+				order.buyer_id = Buyer.objects.all().get(email = data['email'])
+				order.sock_id = Sock.objects.all().get(sock_id = data['sock_id'])
+				order.charity_id = Charity.objects.all().get(charity_id = data['charity_id'])
+				order.delivery_id = Delivery.objects.all().get(delivery_id = data['delivery_id'])
 				order.save()
 				form.save()
 			else:
-				existing_info = Buyer.objects.all().filter(email = data['email'])
-				print existing_info
-				existing_buyer_id = existing_info.values_list('buyer_id', flat = True)
-				print existing_buyer_id
-				#These return querysets for the correct buyer instance. 
-				#Cant parse it to retrieve the value. 
-				#Could maybe go back to foreign keys using this as a template.
-				#They technically return instances of models, could work I guess...
-				#Find the buyer's id because it exists and use that to populate other tables
-				buyerRole.buyer_id = existing_buyer_id #Here
-				buyerRole.role_id = data['role_id']
-				buyerRole.save()
+				existing_info = Buyer.objects.all().get(email = data['email'])
 				if data['delivery_id'] == 2:
-					buyerDelInfo.buyer_id = existing_buyer_id #Here
+					buyerDelInfo.buyer_id = existing_info
 					buyerDelInfo.building_name = data['building_name']
 					buyerDelInfo.room_number = data['room_number']
 					buyerDelInfo.save()
-				order.buyer_id = existing_buyer_id #Here
-				order.sock_id = data['sock_id']
-				order.charity_id = data['charity_id']
-				order.delivery_id = data['delivery_id']
+				order.buyer_id = existing_info
+				order.sock_id = Sock.objects.all().get(sock_id = data['sock_id'])
+				order.charity_id = Charity.objects.all().get(charity_id = data['charity_id'])
+				order.delivery_id = Delivery.objects.all().get(delivery_id = data['delivery_id'])
 				order.save()
 				form.save()
 			form = OrderForm()
