16e30d546b1857d44f311772dcf649643810886b
Andrew Smith <andrw.l.smth@gmail.com>
2017-04-27 19:39:05 -0400

    Clean up choices.py

--- GiftOfSocks/choices.py
+++ GiftOfSocks/choices.py
@@ -1,31 +1,29 @@
 choices = (
-	        (1, 'ESU Colors - Medium - Men (6-8) Donation $20'),
-	        (2, 'ESU Colors - Large - Men (9-13) Donation $20'), 
-	        (3, 'Neon Colors - Medium - Men (6-8) Donation $20'), 
-	        (4, 'Neon Colors - Large - Men (9-13) Donation $20'), 
-	        (5, 'ESU Colors - Medium - Women (6-9) Donation $20'), 
-	        (6, 'ESU Colors - Large - Women (10-13) Donation $20'),
-	        (7, 'Neon Colors - Medium - Women (6-9) Donation $20'), 
-	        (8, 'Neon Colors - Large - Women (10-13) Donation $20')
-	    )
+	(1, 'ESU Colors - Medium - Men (6-8) Donation $20'),
+	(2, 'ESU Colors - Large - Men (9-13) Donation $20'), 
+	(3, 'Neon Colors - Medium - Men (6-8) Donation $20'), 
+	(4, 'Neon Colors - Large - Men (9-13) Donation $20'), 
+	(5, 'ESU Colors - Medium - Women (6-9) Donation $20'), 
+	(6, 'ESU Colors - Large - Women (10-13) Donation $20'),
+	(7, 'Neon Colors - Medium - Women (6-9) Donation $20'), 
+	(8, 'Neon Colors - Large - Women (10-13) Donation $20')
+)
 
 choices2 = (
-	    (1, 'Student'),
-	    (2, 'Community Member'), 
-	    (3, 'Staff'), 
-	    (4, 'Faculty'), 
-	    (5, 'Administrator'), 
-	    (6, 'Alumni'),
-	)
-
+	(1, 'Student'),
+	(2, 'Community Member'), 
+	(3, 'Staff'), 
+	(4, 'Faculty'), 
+	(5, 'Administrator'), 
+	(6, 'Alumni'),
+)
 
 choices3 = (
-	    (1, 'Street2Feet'),
-	    (2, 'The Good Project'), 
-	)
-
+	(1, 'Street2Feet'),
+	(2, 'The Good Project'), 
+)
 
 choices4 = (
-	    (1, 'I want to pick up my Socks. Socks are available for Pickup from the C.R.E.A.T.E. Lab (Stroud Hall, Room 107) Tuesdays & Wednesdays 1-4pm'),
-	    (2, 'I want my socks delivered. Delivery available on campus only. Delivery day is any Tuesday between 1-4pm'), 
-	)
+	(1, 'I want to pick up my Socks. Socks are available for Pickup from the C.R.E.A.T.E. Lab (Stroud Hall, Room 107) Tuesdays & Wednesdays 1-4pm'),
+	(2, 'I want my socks delivered. Delivery available on campus only. Delivery day is any Tuesday between 1-4pm'), 
+)
