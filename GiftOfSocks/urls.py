489ca0e8e6f0bc8585f249e23a81c438c6fd074f
Andrew Smith <andrw.l.smth@gmail.com>
2017-04-27 19:40:40 -0400

    Remove uneeded templates and urls

--- GiftOfSocks/urls.py
+++ GiftOfSocks/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import *
 from django.contrib import admin
-from form.views import user_form, admin_form, user, admin_shell, admin_user_results, admin_new_charity
+from form.views import user, admin_shell, admin_user_results, admin_new_charity
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^user_form/$',user_form, name='user_form'),
-    url(r'^admin_form/$',admin_form, name='admin_form'),
+    url(r'^admin/', include(admin.site.urls)), #Django default admin page. NOT USED
     url(r'^user/$',user, name='user'),
     url(r'^admin_shell/$',admin_shell, name='admin_shell'),
     url(r'^admin_user_results/$',admin_user_results, name='admin_user_results'),
